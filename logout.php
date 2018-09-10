116d1fa0756d1e873884f76625a0ba444a1d03ba
lzag <lzagroba@gmx.com>
2018-09-10 13:21:42 +0200

    Modifies the logout page \nHi from the template commit-msg hook

--- logout.php
+++ logout.php
@@ -2,15 +2,20 @@
 
 require_once 'header.php';
 
-if (isset($_SESSION['user']))
-{
-    destroySession();
-    echo "<div class='main'>You have been logged out. Please "."<a href='index.php'>click here</a>to refresh the screen.";
-}
-else echo "<div class='main'><br>". "You cannot log out because you are not logged in";
+if (isset($_SESSION['user'])) :
+
+	destroySession();
 
 ?>
 
-<br><br></div>
-</body>
-</html>
+<span class="text-secondary mt-3">You have been logged out. Please <a href="index.php">click here</a> to refresh the screen.</span>
+
+<?php else : ?>
+
+<span class="text-danger mt-3">You cannot log out because you are not logged in.</span>
+
+<?php
+
+endif;
+
+require 'footer.php'; ?>
